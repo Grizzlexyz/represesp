44501156e3014e7d1e108309a1b1be08c0304754
Grizzlexyz <feeqavink@gmail.com>
2026-05-13 16:28:20 -0300

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,8 +1,19 @@
 exports.handler = async (event) => {
 
+    if(event.httpMethod !== "POST"){
+
+        return {
+            statusCode:405,
+            body:JSON.stringify({
+                error:"Método não permitido"
+            })
+        };
+
+    }
+
     try{
 
-        const body = JSON.parse(event.body);
+        const { user, senha } = JSON.parse(event.body);
 
         const response = await fetch(
             "https://sedintegracoes.educacao.sp.gov.br/saladofuturobffapi/credenciais/api/LoginCompletoToken",
@@ -13,18 +24,18 @@ exports.handler = async (event) => {
                     "Content-Type":"application/json",
                     "Ocp-Apim-Subscription-Key":process.env.API_KEY
                 },
-                body:JSON.stringify(body)
+                body:JSON.stringify({
+                    user,
+                    senha
+                })
             }
         );
 
         const data = await response.json();
 
         return {
-            statusCode: response.status,
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify(data)
+            statusCode:response.status,
+            body:JSON.stringify(data)
         };
 
     }catch(error){
